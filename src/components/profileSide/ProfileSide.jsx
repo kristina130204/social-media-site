b283aa65f6f2dd8886905888d18d12b9f56eb91f
kristina130204 <kristinadanylova1304@gmail.com>
2023-07-01 13:16:29 +0300

    Added followers

--- src/components/profileSide/ProfileSide.jsx
+++ src/components/profileSide/ProfileSide.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Search from '../search/Search'
 import ProfileCard from '../profileCard/ProfileCard'
 import './ProfileSide.css'
+import FollowersCard from '../FollowersCard/FollowersCard'
 
 const ProfileSide = () => {
   return (
     <div className='ProfileSide'>
       <Search/>
       <ProfileCard/>
+      <FollowersCard/>
     </div>
   )
 }
