e78ee9c63e99fbc9ac7efd18cea64f9d89cf07f2
kristina130204 <kristinadanylova1304@gmail.com>
2023-07-08 15:49:32 +0300

    Auth functionality added on backend

---
+++ server/Controllers/AuthController.js
@@ -0,0 +1,33 @@
+import UserModel from "../Models/userModel.js";
+import bcrypt from 'bcrypt';
+
+export const register = async(req, res) => {
+    const {username, password, firstName, lastName} = req.body;
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(password, salt);
+    const newUser = new UserModel({username, password:hashedPass, firstName, lastName});
+
+    try {
+        await newUser.save();
+        res.status(200).json(newUser);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
+
+
+export const login = async(req, res) => {
+    const {username, password} = req.body;
+
+    try {
+        const user = await UserModel.findOne({username:username});
+        if(user){
+            const validity = await bcrypt.compare(password, user.password);
+            validity ? res.status(200).json(user) : res.status(400).json("Wrong password.");
+        } else{
+            res.status(404).json("User does not exists.");
+        }
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+}
